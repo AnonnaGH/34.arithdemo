bd2273c3acdc75a60e8af864894393713985456c
Anonna Ferdaus <44522472+AnonnaGH@users.noreply.github.com>
2021-03-04 17:52:36 +0600

    Update main.go

---
+++ main.go
@@ -0,0 +1,37 @@
+package main 
+ 
+ import "fmt"
+ 
+ func main(){
+        
+	var a,b int
+	
+	a=5
+	b=10
+	
+	
+	// arithmetic operation
+	// addition
+	c := a + b
+	fmt.Printf("%d + %d = %d \n",a,b,c)
+	
+	// subtraction
+	d := a-b
+	fmt.Printf("%d - %d = %d \n",a,b,d)
+	
+	
+
+	// division
+         e := float32(a) / float32(b)
+         fmt.Printf("%d / %d = %.2f \n",a,b,e)
+
+
+        // multiplication
+        f := a * b
+        fmt.Printf("%d * %d = %d \n",a,b,f)
+
+ 
+ 
+ 
+ 
+ }
